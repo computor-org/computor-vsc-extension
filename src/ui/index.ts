b6792cda5e4fa09c3e6f2b5b31a5b4e95f33d2fa
Maximilian Mandlez <maximilian.mandlez@student.tugraz.at>
2025-07-19 13:40:26 +0200

    Update UI framework to use JavaScript components

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -5,17 +5,5 @@ export { BaseView } from './base/BaseView';
 export { BaseWebviewPanel } from './base/BaseWebviewPanel';
 export { BaseTreeDataProvider } from './base/BaseTreeDataProvider';
 
-// Components
-export * from './components';
-
 // Types
 export * from './types';
-
-// Registry
-export { ViewRegistry, viewRegistry } from './registry/ViewRegistry';
-
-// State management
-export { ViewStateManager, createViewState } from './state/ViewStateManager';
-
-// Styles
-export { designTokens } from './styles/tokens';
