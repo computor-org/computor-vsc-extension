59b85e1b797de06218a81cdf6727cc05e1bba50a
Maximilian Mandlez <maximilian.mandlez@student.tugraz.at>
2025-07-19 19:34:27 +0200

    Add authentication module exports

---
+++ src/authentication/index.ts
@@ -0,0 +1,5 @@
+export { ComputorAuthenticationProvider } from './ComputorAuthenticationProvider';
+export { VscodeCredentialStorage } from './VscodeCredentialStorage';
+export { ComputorCredentialManager, CredentialStorageError, CredentialNotFoundError, TokenExpiredError } from './ComputorCredentialManager';
+export { TokenManager } from './TokenManager';
+export * from '../types/AuthenticationTypes';
