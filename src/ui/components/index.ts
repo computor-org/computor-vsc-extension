e3f34307d18fd5bfac3bd8c5c752496526ea8e2d
Maximilian Mandlez <maximilian.mandlez@student.tugraz.at>
2025-07-19 13:40:14 +0200

    Remove React components and unused UI infrastructure

--- src/ui/components/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all components from here
-export * from './Button';
-export * from './Input';
-export * from './Card';
-export * from './Select';
-export * from './Checkbox';
-export * from './Radio';
-export * from './Progress';
